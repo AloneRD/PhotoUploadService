4b5d348f623d8852fb37ba65e9b604a44300240e
Artur <artur_goncharov_97@mail.ru>
2022-10-24 22:17:51 +0300

    added async pack

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import asyncio
 import os
-from subprocess import check_output
 from typing import NoReturn
 
 from dotenv import load_dotenv
@@ -7,21 +7,31 @@ from dotenv import load_dotenv
 
 def save_file(byte_archive: bytes, archive_name:str) -> NoReturn:
     '''Сохраняет заархивированные данные в файл'''
-    with open('archive.zip', 'wb') as archive:
+    with open(archive_name, 'ab') as archive:
         archive.write(byte_archive)
 
 
-def pack(files_path: str, archive_name: str) -> NoReturn:
+async def pack(files_path: str, archive_name: str) -> NoReturn:
     '''Упаковывает файлы в архив'''
-    archive = check_output(['zip', '-r', '-', files_path])
-    save_file(archive, archive_name)
+    cmd = f'zip -r - {files_path}'
+    process = await asyncio.subprocess.create_subprocess_exec(
+        'zip',
+        '-r',
+        '-',
+        files_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    while  not process.stdout.at_eof(): 
+        stdout = await process.stdout.read(100*1000)
+        save_file(stdout, 'first.zip')
 
 
 def main():
     load_dotenv()
     files_path = os.getenv('ARCHIVE_FILES_SAVE_PATH')
     archive_name = "first.zip"
-    pack(files_path, archive_name)
+    asyncio.run(pack(files_path, archive_name))
 
 
 if __name__ == '__main__':
